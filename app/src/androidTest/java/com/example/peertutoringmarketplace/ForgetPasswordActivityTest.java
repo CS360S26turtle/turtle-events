fd955acf96c36e8ce92188c19607481b734b4e77
Maha Shabbir <138251466+m-shab@users.noreply.github.com>
2026-04-06 23:09:53 +0500

    Update tests to scroll before clicking buttons

--- app/src/androidTest/java/com/example/peertutoringmarketplace/ForgetPasswordActivityTest.java
+++ app/src/androidTest/java/com/example/peertutoringmarketplace/ForgetPasswordActivityTest.java
@@ -12,6 +12,7 @@ import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 
+//The following test is from ChatGPT, "Generate tests for ForgetPassword UI", 2026-04-06
 @RunWith(AndroidJUnit4.class)
 public class ForgetPasswordActivityTest {
 
@@ -22,14 +23,14 @@ public class ForgetPasswordActivityTest {
     @Test
     public void testInvalidEmail() {
         onView(withId(R.id.emailEditText)).perform(typeText("invalid"), closeSoftKeyboard());
-        onView(withId(R.id.resetButton)).perform(click());
+        onView(withId(R.id.resetButton)).perform(scrollTo(), click());
         onView(withId(R.id.emailEditText))
                 .check(matches(hasErrorText("Please enter a valid email address")));
     }
 
     @Test
     public void testBackToLogin() {
-        onView(withId(R.id.backToLoginText)).perform(click());
+        onView(withId(R.id.backToLoginText)).perform(scrollTo(), click());
         onView(withId(R.id.loginButton)).check(matches(isDisplayed()));
     }
 }
