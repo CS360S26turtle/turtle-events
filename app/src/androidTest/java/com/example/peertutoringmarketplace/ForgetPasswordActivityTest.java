12fdc0699735551958accbf02d6674939e8ee2a7
Maha Shabbir <138251466+m-shab@users.noreply.github.com>
2026-04-06 22:43:29 +0500

    Remove email validation tests from ForgetPasswordActivityTest
    Removed tests for empty email and invalid email format from ForgetPasswordActivityTest.

--- app/src/androidTest/java/com/example/peertutoringmarketplace/ForgetPasswordActivityTest.java
+++ app/src/androidTest/java/com/example/peertutoringmarketplace/ForgetPasswordActivityTest.java
@@ -7,31 +7,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-
-@Test
-public void testEmptyEmailShowsError() {
-    onView(withId(R.id.passwordEditText)).perform(typeText("123456"), closeSoftKeyboard());
-    onView(withId(R.id.loginButton)).perform(click());
-    onView(withId(R.id.emailEditText))
-            .check(matches(hasErrorText("Email is required")));
-}
-
-@Test
-public void testInvalidEmailFormat() {
-    onView(withId(R.id.emailEditText)).perform(typeText("invalid"), closeSoftKeyboard());
-    onView(withId(R.id.passwordEditText)).perform(typeText("123456"), closeSoftKeyboard());
-    onView(withId(R.id.loginButton)).perform(click());
-    onView(withId(R.id.emailEditText))
-            .check(matches(hasErrorText("Please enter a valid email address")));
-}
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 
-//The following test is from ChatGPT, "Generate tests for ForgetPassword UI", 2026-04-06
 @RunWith(AndroidJUnit4.class)
 public class ForgetPasswordActivityTest {
 
