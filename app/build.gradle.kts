d38a45fc76f9cf8a3a6dfb97ba094c30d86ca9ad
Shanzay2000 <27100113@lums.edu.pk>
2026-04-26 14:16:55 +0500

    Group sessions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,7 @@ dependencies {
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    // Kizitonwose Calendar View
+    implementation("com.kizitonwose.calendar:view:2.5.4")
 }
